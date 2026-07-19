a71c5255dac9766de71b076c8c441b5c4188b4b1
Tomasz Guzik <tguzik@redhat.com>
2026-07-19 13:09:15 +0200

    refactor: add pydantic models, refactor the human.json

---
+++ models/__init__.py
@@ -0,0 +1,37 @@
+from models.action import (
+    Action,
+    AddAttribute,
+    AddItem,
+    AddLanguage,
+    AddProfession,
+    Choice,
+    GrantLiteracy,
+)
+from models.ancestry import AncestryData, GeneralStats
+from models.base_hero import AncestryHero
+from models.equipment import Armor, Equipment, Money, Shield, Weapon
+from models.language import Language
+from models.spell import Spell, Tradition
+from models.talent import Talent
+
+__all__ = [
+    "Action",
+    "AddAttribute",
+    "AddItem",
+    "AddLanguage",
+    "AddProfession",
+    "AncestryData",
+    "AncestryHero",
+    "Armor",
+    "Choice",
+    "Equipment",
+    "GeneralStats",
+    "GrantLiteracy",
+    "Language",
+    "Money",
+    "Shield",
+    "Spell",
+    "Talent",
+    "Tradition",
+    "Weapon",
+]
